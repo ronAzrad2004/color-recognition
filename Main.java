a927ba83e6638a19e8a71f534d2478ca8f6ac596
ronAzrad2004 <ron12azrad@gmail.com>
2025-04-02 18:32:05 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,13 +8,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[]args){
         Scanner scanner=new Scanner(System.in);
-        List<String> list= new ArrayList<>();
-        list.add("Red");
-        list.add("Lime");
-        list.add("Blue");
-        list.add("White");
-        list.add("Black");
-        list.add("Yellow");
         System.out.print("enter the path of the image you want to scan the color:");
         String str=Project_code.starts(scanner.nextLine());
         System.out.print("The color is:"+str);
