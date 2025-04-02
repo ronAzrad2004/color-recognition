722720b00fcd85d28672361af6a5b51387d124db
ronAzrad2004 <ron12azrad@gmail.com>
2025-04-02 18:34:29 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 package unitests;
 import com.company.*;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-
+/**
+ * Main class to run the program and determine the color from an image.
+ */
 public class Main {
     public static void main(String[]args){
         Scanner scanner=new Scanner(System.in);
+
         System.out.print("enter the path of the image you want to scan the color:");
+
+        // Read the file path from user input and pass it to the Project_code class for processing
         String str=Project_code.starts(scanner.nextLine());
+        
         System.out.print("The color is:"+str);
     }
 }
