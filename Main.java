ac5d0fea54538ad42a67b383e7b49c8933e166d5
ronAzrad2004 <ron12azrad@gmail.com>
2025-04-02 18:36:36 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.print("enter the path of the image you want to scan the color:");
 
         // Read the file path from user input and pass it to the Project_code class for processing
-        String str=Project_code.starts(scanner.nextLine());
+        String str=Project_code.findColName(scanner.nextLine());
         
         System.out.print("The color is:"+str);
     }
